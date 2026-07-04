d02c4c9ea6abc3917b24bccae86a262e4b3026d7
JustFun <paw93@ukr.net>
2026-07-04 16:27:16 +0300

    reviews

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './js/burgerMenu';
-import './js/how-to-play-swiper';
 import './js/gameplay-swiper';
 import './js/feature-info-swiper';
 import './js/feature-details-swiper';
+import './js/reviews-swiper';
